2179e4410cb65300f8107e1a621b37987d977925
Matheus Lima <lima.2002@outlook.com>
2025-01-14 07:07:13 -0300

    01exercise-14-01-2025

--- 02 - Lógica de Programação - Funções e Listas (Alura)/01 - Arquivos/js-curso-2-aula1/app.js
+++ 02 - Lógica de Programação - Funções e Listas (Alura)/01 - Arquivos/js-curso-2-aula1/app.js
@@ -1,4 +1,4 @@
-let numeroSecreto = gerarNumeroAleatorio ();
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
 let campo = document.querySelector(tag);
@@ -10,7 +10,18 @@ exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
     let chute = document.querySelector("input").value;
-    console.log(chute == numeroSecreto);
+    
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        exibirTextoNaTela("p", "Você descobriu o número secreto!");
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela ("p", "O número secreto é menor");
+        } else {
+            exibirTextoNaTela("p", "O número secreto é maior");
+        }
+    }
+
 }
 
 function gerarNumeroAleatorio() {
