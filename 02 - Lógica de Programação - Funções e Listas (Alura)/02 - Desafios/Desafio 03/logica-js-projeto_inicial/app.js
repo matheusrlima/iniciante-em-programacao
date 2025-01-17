8fbd31bf5d6e0b610513c9c5c2da5a46d3b81e66
Matheus Lima <lima.2002@outlook.com>
2025-01-16 21:19:26 -0300

    challenge-16-01-2025

--- 02 - Lógica de Programação - Funções e Listas (Alura)/02 - Desafios/Desafio 03/logica-js-projeto_inicial/app.js
+++ 02 - Lógica de Programação - Funções e Listas (Alura)/02 - Desafios/Desafio 03/logica-js-projeto_inicial/app.js
@@ -1,6 +1,26 @@
-peso = 0.0;
-altura = 0.0;
+let peso = 0.0;
+let altura = 0.0;
+let valorIMC = 0.0;
 
 function calcularIMC () {
-    peso = prompt("Digite seu peso");
+    peso = prompt("Digite seu peso:");
+    altura = prompt("Digite sua altura:");
+    
+    valorIMC = peso / (altura * altura);
+    alert(`Seu índice de massa corporal (IMC) é ${valorIMC}`);
+
 }
+
+function fatorial () {
+    let valorFatorial = parseInt(prompt("Digite um valor que deseja saber seu fatorial:"));
+    let resultFatorial = 1;
+
+    for (let i = valorFatorial; i > 1; i--) {
+        resultFatorial *= i;
+    }
+
+    alert(`O valor de ${valorFatorial}! é de ${resultFatorial}`);
+
+}
+
+fatorial();
