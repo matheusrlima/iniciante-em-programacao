c298eef81d0aca3e997659dca8319b76b7c33a54
Matheus Lima <lima.2002@outlook.com>
2025-01-25 11:07:51 -0300

    Update app.js

---
+++ 02 - Lógica de Programação - Funções e Listas (Alura)/02 - Desafios/Desafio 04/logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+let listaGenerica = [];
+let linguagensDeProgramação = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+linguagensDeProgramação.push("Java", "Ruby", "GoLang");
+
+console.log (linguagensDeProgramação[0]);
+console.log (linguagensDeProgramação[1]);
+console.log (linguagensDeProgramação[linguagensDeProgramação.length - 1])
